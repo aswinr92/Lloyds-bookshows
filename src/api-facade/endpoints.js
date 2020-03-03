77351913e48ed87617e0d80d2557269c7abfba30
Aswin Radhakrishnan <aswradha@publicisgroupe.net>
2020-03-03 11:53:21 +0530

    Update endpoints

--- src/api-facade/endpoints.js
+++ src/api-facade/endpoints.js
@@ -1,4 +1,4 @@
 export const moviesEndpoint =
-  "http://www.omdbapi.com/?apikey=4b6c78de&s=of%20the&y=2020&type=movie";
+  "https://www.omdbapi.com/?apikey=4b6c78de&s=of%20the&y=2020&type=movie";
 
-export const detailsEndpoint = "http://omdbapi.com/?apikey=4b6c78de&i=";
+export const detailsEndpoint = "https://omdbapi.com/?apikey=4b6c78de&i=";
